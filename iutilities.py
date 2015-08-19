32fdf683b2e9c63477b9dce40bfdf5e8d69c14dd
Qurban <qurban_ali36@yahoo.com>
2015-08-19 15:15:25 +0500

    onerror function added

--- iutilities.py
+++ iutilities.py
@@ -43,6 +43,25 @@ class memoize(object):
    def __get__(self, obj, objtype):
       '''Support instance methods.'''
       return functools.partial(self.__call__, obj)
+  
+def onerror(func, path, exc_info):
+    """
+    Error handler for ``shutil.rmtree``.
+
+    If the error is due to an access error (read only file)
+    it attempts to add write permission and then retries.
+
+    If the error is for another reason it re-raises the error.
+
+    Usage : ``shutil.rmtree(path, onerror=onerror)``
+    """
+    import stat
+    if not os.access(path, os.W_OK):
+        # Is the error an access error ?
+        os.chmod(path, stat.S_IWUSR)
+        func(path)
+    else:
+        raise
 
 def get_image_size(fname):
     '''Determine the image type of fhandle and return its size.
