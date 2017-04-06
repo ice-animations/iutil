f5a6e5b49a657d37ac71dbd5f2d3092e5e548403
Talha Ahmed <talha.ahmed@gmail.com>
2017-04-06 14:11:58 +0500

    getTemp now returns fullPathNames

--- iutilities.py
+++ iutilities.py
@@ -262,11 +262,51 @@ def isTempPath(path):
         return True
     return False
 
+import ctypes
+from ctypes import wintypes
+
+_GetShortPathNameW = windll.kernel32.GetShortPathNameW
+_GetShortPathNameW.argtypes = [wintypes.LPCWSTR, wintypes.LPWSTR, wintypes.DWORD]
+_GetShortPathNameW.restype = wintypes.DWORD
+
+def getShortPathName(long_name):
+    """
+    Gets the short path name of a given long path.
+    http://stackoverflow.com/a/23598461/200291
+    """
+    output_buf_size = 0
+    while True:
+        output_buf = ctypes.create_unicode_buffer(output_buf_size)
+        needed = _GetShortPathNameW(long_name, output_buf, output_buf_size)
+        if output_buf_size >= needed:
+            return output_buf.value
+        else:
+            output_buf_size = needed
+
+_GetLongPathNameW = windll.kernel32.GetLongPathNameW
+_GetLongPathNameW.argtypes = [wintypes.LPCWSTR, wintypes.LPWSTR, wintypes.DWORD]
+_GetLongPathNameW.restype = wintypes.DWORD
+
+def getLongPathName(long_name):
+    """
+    Gets the short path name of a given long path.
+    http://stackoverflow.com/a/23598461/200291
+    """
+    output_buf_size = 0
+    while True:
+        output_buf = ctypes.create_unicode_buffer(output_buf_size)
+        needed = _GetLongPathNameW(long_name, output_buf, output_buf_size)
+        if output_buf_size >= needed:
+            return output_buf.value
+        else:
+            output_buf_size = needed
+
 def getTemp(mkd = False, suffix = "", prefix = "tmp", directory = None):
     tmp = getattr(tempfile,
                   "mkdtemp" if mkd else "mkstemp")(suffix = suffix,
                                                              prefix = prefix,
                                                              dir = directory)
+    tmp = tmp[0], getLongPathName(unicode(tmp[1]))
     if mkd: return tmp
     else:
         os.close(tmp[0])
